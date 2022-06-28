b1026689eebcd1688060dbe6f4105024991e775a
RYLiang18 <richard.y.liang@gmail.com>
2022-06-28 14:36:54 -0700

    dev: experience/create_experience done

--- flask_app/experience/forms.py
+++ flask_app/experience/forms.py
@@ -18,11 +18,13 @@ def is_date(data):
     regex = r"(^[\d]{2}/[\d]{2}/[\d]{4}$)|(^present$)"
     return bool(re.search(regex, data))
 
-class CreateExperience(FlaskForm):
-    company_name = StringField("Company Name", validators=[InputRequired])
-    start_date = StringField("Start Date", validators=[InputRequired])
-    end_date = StringField("End Date", validators=[InputRequired])
-    about = StringField("About Experience", validators=[InputRequired])
+class CreateExperienceForm(FlaskForm):
+    company_name = StringField("Company Name", validators=[InputRequired()])
+    position = StringField("Position", validators=[InputRequired()])
+    start_date = StringField("Start Date", validators=[InputRequired()])
+    end_date = StringField("End Date", validators=[InputRequired()])
+    about = TextAreaField("About Experience", validators=[InputRequired()])
+    submit = SubmitField("Submit")
 
     def validate_start_date(form, field):
         if not is_date(field.data):
--- flask_app/experience/routes.py
+++ flask_app/experience/routes.py
@@ -1,10 +1,17 @@
 from flask import (
     Blueprint,
-    render_template
+    render_template,
+    redirect,
+    url_for
 )
+from flask_app.experience.forms import CreateExperienceForm
 from flask_login import(
     login_required,
     current_user
+)
+from flask_app.models import (
+    Experience,
+    load_user
 )
 
 experience_blueprint = Blueprint("experience", __name__, url_prefix="/experience", template_folder='./templates')
@@ -14,4 +21,28 @@ experience_blueprint = Blueprint("experience", __name__, url_prefix="/experience
 def index():
     return render_template(
         "experience.html", title=f"{current_user.username}\'s experience"
+    )
+
+@experience_blueprint.route("/create_experience", methods=["GET", "POST"])
+@login_required
+def create_experience():
+    create_experience_form = CreateExperienceForm()
+
+    if create_experience_form.validate_on_submit():
+        new_experience = Experience(
+            owner = load_user(current_user.username),
+            company_name = create_experience_form.company_name.data,
+            position = create_experience_form.position.data,
+            start_date = create_experience_form.start_date.data,
+            end_date = create_experience_form.end_date.data,
+            about = create_experience_form.about.data
+        )
+        new_experience.save()
+
+        return redirect(url_for("experience.index"))
+
+    return render_template(
+        "create_experience.html",
+        title = "Experience - Create Experience",
+        form = create_experience_form
     )
