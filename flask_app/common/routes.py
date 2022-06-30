4ec8e26468bc930c314173dbb66590812a09b800
RYLiang18 <richard.y.liang@gmail.com>
2022-06-29 17:16:07 -0700

    dev: unpushed changes from last commit

--- flask_app/common/routes.py
+++ flask_app/common/routes.py
@@ -11,7 +11,6 @@ from flask_login import(
     current_user
 )
 from flask_app.models import (
-    HomepageDetails,
     HomepageDetailsLink,
     load_user,
     Link
@@ -25,3 +24,32 @@ from flask_app.homepage.forms import (
     AboutMeUpdateForm,
     EmailUpdateForm
 )
+
+common_blueprint = Blueprint(
+    "common", __name__, url_prefix="/common", template_folder="./templates"
+)
+
+def matching_username(username):
+    return current_user.username == username
+
+@homepage_blueprint.route(
+    "/update_link/<link_owner_username>/<link_creation_time>", methods=["GET", "POST"]
+)
+@login_required
+def update_link(link_owner_username, link_creation_time):
+    if not matching_username(link_owner_username):
+        flash("You can\'t edit someone else\'s link!")
+        return redirect(url_for('homepage.index'))
+    
+    link_collections = [HomepageDetailsLink]
+    
+    link = None
+    for link_collection in link_collections:
+        link = link_collection.objects(
+            owner=load_user(link_owner_username),
+            creation_time = link_creation_time
+        ).first()
+
+        if link is not None:
+            break
+    
