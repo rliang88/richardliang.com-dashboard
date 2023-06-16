bbf8af1cc0eeb21e06d85f7fe263671ebcb2a590
Richard Liang <113130894+rliang88@users.noreply.github.com>
2023-06-16 00:33:02 -0700

    [PWAD-19] homepage pfp and full name using submit_simple_content.html (#16)

--- flask_app/homepage/forms.py
+++ flask_app/homepage/forms.py
@@ -1,45 +1,41 @@
 import re
+
 from flask_wtf import FlaskForm
-from wtforms import (
-    StringField,
-    SubmitField,
-    TextAreaField
-)
-from wtforms.fields import (
-    URLField,
-    EmailField
-)
-from wtforms.validators import (
-    InputRequired,
-    ValidationError
-)
+from wtforms import StringField, SubmitField, TextAreaField
+from wtforms.fields import EmailField, URLField
+from wtforms.validators import InputRequired, ValidationError
+
 
 class FullNameUpdateForm(FlaskForm):
     full_name = StringField("Full Name", validators=[InputRequired()])
     update = SubmitField("Update")
 
-# Hello World
+
 class EmailUpdateForm(FlaskForm):
-    email = EmailField("Email", validators=[InputRequired()])
-    update = SubmitField("Update")
+    content = EmailField("Email", validators=[InputRequired()])
+    submit = SubmitField("Update")
+
 
 class PFPLinkUpdateForm(FlaskForm):
-    url = URLField(
-        "Image URL (ending in .jpg, .png, etc...)",
-        validators = [InputRequired()]
+    content = URLField(
+        "Image URL (ending in .jpg, .png, etc...)", validators=[InputRequired()]
     )
-    update = SubmitField("Update")
+    submit = SubmitField("Update")
 
     def validate_url(form, field):
         jpg_regex = r"\.jpg$"
         png_regex = r"\.png$"
 
-        if (not re.search(jpg_regex, field.data)) and (not re.search(png_regex, field.data)):
+        if (not re.search(jpg_regex, field.data)) and (
+            not re.search(png_regex, field.data)
+        ):
             raise ValidationError("URL must end in .jpg or .png")
+
 
 class DescriptionUpdateForm(FlaskForm):
     description = TextAreaField("Description", validators=[InputRequired()])
     update = SubmitField("Update")
+
 
 class AboutMeUpdateForm(FlaskForm):
     about_me = TextAreaField("About Me", validators=[InputRequired()])
--- flask_app/homepage/routes.py
+++ flask_app/homepage/routes.py
@@ -7,7 +7,6 @@ from flask_app.homepage.forms import (
     AboutMeUpdateForm,
     DescriptionUpdateForm,
     EmailUpdateForm,
-    FullNameUpdateForm,
     PFPLinkUpdateForm,
 )
 from flask_app.models import HomepageDetails, Link, load_user  # Link
@@ -32,33 +31,12 @@ def index():
     ).first()
 
     homepage_details_links = Link.objects(parent=homepage_details)
-    # homepage_details_links = HomepageDetailsLink.objects(
-    #     owner=load_user(current_user.username)
-    # )
 
     return render_template(
         "homepage.html",
         title=f"{current_user.username}'s homepage",
         homepage_details=homepage_details,
         links=homepage_details_links,
-    )
-
-
-# deprecated
-@homepage_blueprint.route("/update_full_name", methods=["GET", "POST"])
-@login_required
-def update_full_name():
-    full_name_update_form = FullNameUpdateForm()
-    if full_name_update_form.validate_on_submit():
-        homepage_details = HomepageDetails.objects(owner=current_user).first()
-        homepage_details.update(full_name=full_name_update_form.full_name.data)
-
-        return redirect(url_for("homepage.index"))
-
-    return render_template(
-        "update_full_name.html",
-        form=full_name_update_form,
-        title="Homepage - Update Full Name",
     )
 
 
@@ -69,12 +47,14 @@ def update_email():
 
     if email_update_form.validate_on_submit():
         homepage_details = HomepageDetails.objects(owner=current_user).first()
-        homepage_details.update(email=email_update_form.email.data)
+        homepage_details.update(email=email_update_form.content.data)
 
         return redirect(url_for("homepage.index"))
 
     return render_template(
-        "update_email.html", form=email_update_form, title="Homepage - Update Email"
+        "submit_simple_content.html",
+        form=email_update_form,
+        title="Homepage - Update Email",
     )
 
 
@@ -85,12 +65,12 @@ def update_pfp_link():
 
     if pfp_link_update_form.validate_on_submit():
         homepage_details = HomepageDetails.objects(owner=current_user).first()
-        homepage_details.update(pfp_link=pfp_link_update_form.url.data)
+        homepage_details.update(pfp_link=pfp_link_update_form.content.data)
 
         return redirect(url_for("homepage.index"))
 
     return render_template(
-        "update_pfp_link.html",
+        "submit_simple_content.html",
         form=pfp_link_update_form,
         title="Homepage - Update PFP Link",
     )
@@ -114,90 +94,6 @@ def update_description():
         "update_description.html",
         form=description_update_form,
         title="Homepage - Update Description",
-    )
-
-
-# not in use anymore
-# @homepage_blueprint.route(
-#     "/update_link/<link_owner_username>/<link_datetime_str>", methods=["GET", "POST"]
-# )
-# @login_required
-# def update_personal_link(link_owner_username, link_datetime_str):
-#     # // KEEP OTHERS FROM EDITING YOUR LINKS! ////
-#     if current_user.username != link_owner_username:
-#         flash("You can\'t edit someone else\'s link!")
-#         return redirect(url_for('homepage.index'))
-#     # ////////////////////////////////////////////
-
-#     personal_link = Link.objects(
-#         owner=load_user(link_owner_username), datetime_str = link_datetime_str
-#     ).first()
-
-#     personal_link_update_form = PersonalLinkUpdateForm(
-#         link_name = personal_link.link_name,
-#         url = personal_link.url
-#     )
-
-#     if personal_link_update_form.validate_on_submit():
-#         personal_link.update(
-#             link_name = personal_link_update_form.link_name.data,
-#             url = personal_link_update_form.url.data
-#         )
-
-#         return redirect(url_for('homepage.index'))
-
-#     return render_template(
-#         "update_personal_link.html", form=personal_link_update_form, title="Homepage - Update Personal Link"
-#     )
-
-
-@homepage_blueprint.route(
-    "/delete_link/<link_owner_username>/<link_datetime_str>", methods=["GET"]
-)
-@login_required
-def delete_personal_link(link_owner_username, link_datetime_str):
-    # // KEEP OTHERS FROM EDITING YOUR LINKS! ////
-    if current_user.username != link_owner_username:
-        flash("You can't edit someone else's link!")
-        return redirect(url_for("homepage.index"))
-    # ////////////////////////////////////////////
-
-    # delete the link
-    personal_link = Link.objects(
-        owner=load_user(link_owner_username), datetime_str=link_datetime_str
-    ).first()
-    personal_link_pk = personal_link.pk
-    personal_link.delete()
-
-    # delete the link from homepage_details
-    # there is no cascading delete in mongodb I guess
-    HomepageDetails.objects().update_one(pull__links=personal_link_pk)
-
-    return redirect(url_for("homepage.index"))
-
-
-@homepage_blueprint.route("/add_link", methods=["GET", "POST"])
-@login_required
-def add_personal_link():
-    link_add_form = PersonalLinkAddForm()
-
-    if link_add_form.validate_on_submit():
-        new_link = Link(
-            owner=load_user(current_user.username),
-            link_name=link_add_form.link_name.data,
-            url=link_add_form.url.data,
-            datetime_str=current_time(),
-        )
-        new_link.save()
-
-        homepage_details = HomepageDetails.objects(owner=current_user).first()
-        homepage_details.links.append(new_link)
-        homepage_details.save()
-
-        return redirect(url_for("homepage.index"))
-
-    return render_template(
-        "add_personal_link.html", form=link_add_form, title="Homepage - Add New Link"
     )
 
 
